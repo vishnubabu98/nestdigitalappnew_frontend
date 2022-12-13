d60c2699d6328271ccdffaa2d7531e7cc5481b20
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-13 10:37:04 +0530

    adminlogin

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+
+  username=""
+  password=""
+  constructor(private route:Router){}
+
+  adminLogin=()=>{
+    let data:any={
+
+      "username":this.username,
+      "password":this.password
+
+    }
+    console.log(data)
+
+    if (this.username == "admin" && this.password == "12345") 
+    {
+       this.route.navigate(['/bookentry'])
+    } else 
+    {
+      alert("invalid Credential")
+    }
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
